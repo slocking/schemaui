0a11b61cf0feaa6380e458fac893fe02ad0465d1
molaga <bar@molaga.com>
2021-12-02 19:58:33 +0200

    Add support for sequelize ORM

---
+++ adapters/sequelize.js
@@ -0,0 +1,29 @@
+const Enum = require('../lib/enums');
+const _ = require('lodash');
+
+class SequelizeAdapter {
+    constructor () {
+    }
+
+    getFieldType (field) {
+        // TODO
+    }
+
+    treeToFields (tree = {}, fields = {}, nestedFields = []) {
+        // TODO
+    }
+
+    addOptionsToFields (fields, options = {}) {
+        // TODO
+    }
+
+    parseNewModel (newModel = {}, options) {
+        // TODO
+    }
+
+    addOptionsToModel (model, options = {}) {
+        // TODO
+    }
+}
+
+module.exports = SequelizeAdapter;
