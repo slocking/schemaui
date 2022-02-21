b02d66b22c0f9d422114674499f499df74787b4c
molaga <bar@molaga.com>
2022-02-21 16:50:24 +0200

    WIP

--- adapters/sequelize.js
+++ adapters/sequelize.js
@@ -5,24 +5,62 @@ class SequelizeAdapter {
     constructor () {
     }
 
-    getFieldType (field) {
-        // TODO
-    }
+    treeToFields (tree = {}) {
+        const fields = {};
+        const keys = Object.keys(tree);
 
-    treeToFields (tree = {}, fields = {}, nestedFields = []) {
-        // TODO
+        for (const key of keys) {
+            fields[key] = {
+                key,
+                multi: false,
+                type: Enum.FieldTypes.String,
+                required: false
+            };
+        }
+
+        return fields;
     }
 
     addOptionsToFields (fields, options = {}) {
-        // TODO
+        const fieldKeys = Object.keys(fields);
+
+        for (const key of fieldKeys) {
+            const field = fields[key];
+            const fieldOptions = _.get(options, field.key);
+            if ('object' === typeof fieldOptions && fieldOptions) {
+                Object.assign(field, fieldOptions);
+            }
+        }
     }
 
     parseNewModel (newModel = {}, options) {
-        // TODO
+        const fields = this.treeToFields(newModel.rawAttributes);
+        this.addOptionsToFields(fields, options.fields);
+
+        return {
+            name: newModel.name,
+            fields
+        }
     }
 
     addOptionsToModel (model, options = {}) {
-        // TODO
+        const {
+            listFields = [],
+            permissions = {}
+        } = options;
+
+        const modelPermissions = {
+            [Enum.PermissionType.Read]: true,
+            [Enum.PermissionType.Create]: true,
+            [Enum.PermissionType.Edit]: true,
+            [Enum.PermissionType.Delete]: true,
+            ...permissions,
+        }
+
+        return {
+            listFields,
+            permissions: modelPermissions
+        }
     }
 }
 
--- test/demo/app-sequelize.js
+++ test/demo/app-sequelize.js
@@ -1,3 +1,28 @@
-/* eslint-disable no-console */
+const express = require('express');
+const http = require('http');
+const SchemaUI = require('../../index');
+const User = require('./user-sequelize.model');
+const sequelizeAdapter = require('../../adapters/sequelize');
 
-// TODO
+const port = 4000;
+SchemaUI.init({
+    adapter: sequelizeAdapter
+});
+
+SchemaUI.registerModel(User, {
+    listFields: ['email', 'firstName']
+});
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/schemaui', SchemaUI.middleware());
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
+
+module.exports = server;
